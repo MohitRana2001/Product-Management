f8cd7f967631de5c0aec65fb656a8ec4ae7e238b
MOHIT RANA <awesomemohit20@gmail.com>
2024-11-03 16:48:32 +0530

    Initial Setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <a href="/projects"></a>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
---
+++ src/views/ProjectView.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div>
+    <h1>Project Page</h1>
+    <a href="/"></a>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
